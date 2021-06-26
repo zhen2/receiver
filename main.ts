8bcd8e106b98f117a69ec38992f9e81311578380
zhen2 <zhan3@ncsu.edu>
2021-06-26 11:22:08 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+radio.onReceivedString(function (receivedString) {
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    led.plotBarGraph(
+    Math.map(signal, -95, -42, 0, 9),
+    9
+    )
+})
+let signal = 0
+radio.setGroup(1)
